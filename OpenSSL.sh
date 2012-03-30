7ae46ebcc64525c66e6ecaf7d0239e6e46677a52
knarf <knarf@091d3ff0-52bc-4db5-b7a6-18201e4c0cca>
2012-03-30 17:54:41 +0000

    update to version 1.0.0h. 1.0.1 is out, but would need more testing
    git-svn-id: http://svn.cactuscode.org/projects/ExternalLibraries/OpenSSL/trunk@27 091d3ff0-52bc-4db5-b7a6-18201e4c0cca

--- OpenSSL.sh
+++ OpenSSL.sh
@@ -62,7 +62,7 @@ then
     
     # Set locations
     THORN=OpenSSL
-    NAME=openssl-1.0.0g
+    NAME=openssl-1.0.0h
     SRCDIR=$(dirname $0)
     BUILD_DIR=${SCRATCH_BUILD}/build/${THORN}
     if [ -z "${OPENSSL_INSTALL_DIR}" ]; then
