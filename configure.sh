d83cdc25f49ff9c569fff6589d8f0e789332ba24
Roland Haas <rhaas@illinois.edu>
2023-01-09 11:51:09 -0600

    OpenSSL: update tar file ot 1.1.1s
    this is to support macOS on M1 systems

--- configure.sh
+++ configure.sh
@@ -96,7 +96,7 @@ then
 
     # Set locations
     THORN=OpenSSL
-    NAME=openssl-1.0.1k
+    NAME=openssl-1.1.1s
     SRCDIR="$(dirname $0)"
     BUILD_DIR=${SCRATCH_BUILD}/build/${THORN}
     if [ -z "${OPENSSL_INSTALL_DIR}" ]; then
